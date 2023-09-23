b1b08a9028964e227bd425dd671d1cfb84d66115
Draco1704 <107294328+Draco1704@users.noreply.github.com>
2023-09-22 22:23:33 -0700

    Add files via upload

--- src/paginas/catalogo.jsx
+++ src/paginas/catalogo.jsx
@@ -6,25 +6,57 @@ import DataService from '../servicios/dataService';
 
 function Catalogo(){
     const [productos,setproductos] = useState([]);
+    const [categorias, setCategorias] = useState([]);
+    const [prodsMostrar, setProdsMostrar] = useState([]);
 
+    //carga de componente
     useEffect(function(){
         console.log("componente cargado");
         cargaCatalogo();
     },[]);
 
-    function cargaCatalogo(){
+    function cargaCatalogo()
+    {
     let servicio = new DataService();
     let prods = servicio.getProducts();
     console.log(prods);
     setproductos(prods);
+    let cats= ["Sedan","SUV"];
+    setCategorias(cats);    
+    setProdsMostrar(prods);
     }
+
+function filtro(categorias)
+{
+    console.log(categorias);
+    let lista = [];
+    //encontrar categoria qeu concuerde con producto
+    for (let i=0; i<productos.length; i++)
+    {
+        let prod = productos [i];
+        if (prod.categoria === categorias)
+        {
+            lista.push(prod);
+        }
+
+        console.log(lista);
+        setProdsMostrar(lista);
+    }
+}
+
+function borrarFiltros(){
+    setProdsMostrar(productos);
+
+}
 
     return (
         <div className='catalogo'>
             <h1> Mira nuestos {productos.length} nuevos modelos</h1>
             <br/>
-            {productos.map(p => <Productos data ={p}/>)}
-            
+            <button onClick={borrarFiltros} className='btn btn-dark btn-filter'>Todo</button>
+            {categorias.map(c =><button key ={c} onClick={() => filtro(c)} className='btn btn-sm btn-primary btn-filter'>{c}</button>)}
+            <br/>
+            {prodsMostrar.map(p => <Productos key ={p.id} data={p}/>)}
 
         </div>
     )
